737e69cb5a63af5fad2b55c62568180b8fc9d337
Mithun <mithun@sgcsoft.net>
2023-09-05 02:16:37 +0600

    less than 25 question quiz will not start condition  done

--- app/src/main/java/edu/ewubd/quizzler/QuestionsActivity.java
+++ app/src/main/java/edu/ewubd/quizzler/QuestionsActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -109,8 +110,15 @@ public class QuestionsActivity extends AppCompatActivity {
                 Collections.shuffle(filteredQuestions);            }
         }
 
-        questionAdapter = new QuestionsAdapter(this, filteredQuestions);
-        listQuestion.setAdapter(questionAdapter);
+        if (filteredQuestions.size() == 0) {
+            Toast.makeText(this, "No questions found for this category.", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, CategoryActivity.class);
+            startActivity(intent);
+            finish();
+        } else {
+            questionAdapter = new QuestionsAdapter(this, filteredQuestions);
+            listQuestion.setAdapter(questionAdapter);
+        }
 
         startTimer();
 
